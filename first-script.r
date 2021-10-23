26315f1be8ed44972227d38e647d146da4aebe32
JoeMama <kyledharmasena@gmail.com>
2021-10-23 15:36:11 +0530

    Cleaning up

--- first-script.r
+++ first-script.r
@@ -1,10 +1,10 @@
 name <- "Kyle"
 print (name)
 
-<<<<<<< HEAD
+#HEAD
 input <- "How I Met Your Mother season ending was trash"
 print (input)
-=======
+
+#MASTER
 variable01 <- 3+5
 print (variable01)
->>>>>>> master
